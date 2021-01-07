e6ddbf1e0fc0c4bc5a395607bda87b76667f5330
Carina Hong <carinahong@Carinas-MacBook-Pro.local>
2021-01-07 17:55:56 +0800

    fix typo in player.py

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -11,10 +11,10 @@ import eval7
 import random
 import json
 
-PROBAILITIES={}
+PROBABILITIES={}
 filename="raw_prob.json"
 with open(filename,'r') as load_f:
-    PROBAILITIES = json.load(load_f)
+    PROBABILITIES = json.load(load_f)
 
 #The winning probabilities for RAW first two cards,
 #calculated OUTSIDE this program and stored in prob.json file.
@@ -47,13 +47,13 @@ class Player(Bot):
         num1,num2=card1[0],card2[0]
         suit1,suit2=card1[1],card2[1]
         if num1==num2:
-            return PROBAILITIES[num1+num2]
+            return PROBABILITIES[num1+num2]
         else:
             s1=num1+num2+"s" if suit1==suit2 else num1+num2+"u"
             s2=num2+num1+"s" if suit1==suit2 else num2+num1+"u"
-            if s1 not in PROBAILITIES.keys():
-                return PROBAILITIES[s2]
-            return PROBAILITIES[s1]
+            if s1 not in PROBABILITIES.keys():
+                return PROBABILITIES[s2]
+            return PROBABILITIES[s1]
             
         
     def allocate_cards(self, my_cards):
