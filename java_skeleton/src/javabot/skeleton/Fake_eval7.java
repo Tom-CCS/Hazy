2e0fdff9934dfeb8d595ed72da5b42fc039658cf
Rui Yao <rayyao@mit.edu>
2021-01-13 17:42:04 -0500

    start to implement, changed the file

---
+++ java_skeleton/src/javabot/skeleton/Fake_eval7.java
@@ -0,0 +1,25 @@
+package javabot.skeleton;
+
+import java.util.List;
+
+public class Fake_eval7 {
+	
+	/**
+	 * Calculate the largest combination of the 
+	 * @param ours: our cards
+	 * @param common: the common cards
+	 * @return an integer, the largest score
+	 */
+	public int score(List<String> ours, List<String> common) {
+		return 0;
+	}
+	
+	private int handScore(List<String> cards) {
+		return 0;
+	}
+	
+	public Fake_eval7() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ java_skeleton/src/javabot/skeleton/Fake_eval7_test.java
@@ -0,0 +1,18 @@
+package javabot.skeleton;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class Fake_eval7_test {
+
+	public Fake_eval7_test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
