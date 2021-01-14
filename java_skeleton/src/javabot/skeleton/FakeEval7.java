468a24d24f5d89bfd054bdddd6686968dd064fc3
Rui Yao <rayyao@mit.edu>
2021-01-14 00:46:11 -0500

    finish fake eval 7

--- java_skeleton/src/javabot/skeleton/FakeEval7.java
+++ java_skeleton/src/javabot/skeleton/FakeEval7.java
@@ -1,6 +1,10 @@
 package javabot.skeleton;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -15,24 +19,127 @@ public class FakeEval7 {
 		}
 	}
 	
-	/**
-	 * Calculate the rank of largest combination of the cards
-	 * @param ours: our cards
-	 * @param common: the common cards
-	 * @return an integer, the largest possible score of my cards + common 5 cards
-	 */
-	public int score(List<String> ours, List<String> common) {
-		return 0;
-	}
 	
 	/**
 	 * Calculate the rank of the cards
-	 * @param cards: a set of 5 cards (represented by strings)
+	 * @param ours: our cards (2 strings)
+	 * @param common: the common cards (up to 5 strings)
 	 * @return an integer, the largest possible score of my cards + common 5 cards
 	 */
-	public int handScore(List<String> cards) {
-		return 0;
+	public int score(List<String> ours, List<String> common){
+		int max=0;
+		int n=common.size();
+		List<String> ls=new ArrayList<>(ours);
+		ls.addAll(common);
+		String card1;
+		String card2;
+		int score;
+		for (int i=0;i<n+2;i++) {
+			card1=ls.get(0);
+			ls.remove(0);
+			for (int j=0;j<n+1;j++) {
+				card2=ls.get(0);
+				ls.remove(0);
+				score=this.handScore(ls);
+				if (score>max) max=score;
+				ls.add(card2);
+			}
+			ls.add(card1);
+		}
+		return max;
 	}
+	
+	/**
+	 * Calculate the rank of largest combination of the cards
+	 * @param cards: a set of 5 cards (represented by strings)
+	 * @return an integer, the largest possible score of my cards + common 5 cards
+	 * The score is defined by:
+	 * 10 classes of cards: 
+     * royal_flush=10; straight_flush=9; four_of_a_kind=8; full_house=7;
+     * flush=6; straight=5; three_of_a_kind=4; two_pairs=3; one_pair=2;
+     * high_card=1
+	 * class<<20 + 1st kicker (if any)<<16+ 2nd kicker (if any)<<12 + ... +5th kicker (is any) <<0
+	 */
+	public int handScore(List<String> cards) {
+		int classify=1<<20;
+		int[] kickers=new int[5];
+		for (int i=0;i<5;i++) kickers[i]=1<<(16-4*i);
+	    Map<Integer, Integer> cardDict=toMap(cards);
+	    Set<Integer> ranks=cardDict.keySet();
+	    int straightIndicator=isStraight(cards);
+	    int mag;
+	    if(isFlush(cards)) {
+	        if (straightIndicator>0) {
+	            if (Set.copyOf(cards).equals(Set.of("As","Ks","Qs","Js","Ts"))) return 10*classify;
+	            // A royal flush
+	            else return straightIndicator*kickers[0]+9*classify;
+	            // A straight flush
+	        } else { // a flush
+	            mag=6*classify;
+	            for(int i=0;i<5;i++){
+	                int max=Collections.max(ranks);
+	                ranks.remove(max);
+	                mag=mag+max*kickers[i];
+	            }
+	            return mag;
+	        }
+	    } else {
+	        if (straightIndicator>0) return straightIndicator*kickers[0]+5*classify;
+	        // a straight
+	        else {
+	        	Collection<Integer> values=cardDict.values();
+	            if(values.contains(4)) { // Four of a kind
+	                mag=8*classify;
+	                for(int k:ranks) {
+	                    if (cardDict.get(k)==4) mag+=kickers[0]*k;
+	                    else mag+=kickers[1]*k;}
+	                return mag;
+	            } else if (values.contains(3)) {// 3 of a kind or full house
+	                if (values.contains(2)){// full house
+	                    mag=7*classify;
+                		for(int k:ranks) {
+    	                    if (cardDict.get(k)==3) mag+=kickers[0]*k;
+    	                    else mag+=kickers[1]*k;}
+    	                return mag;
+	                } else { // Three of a kind
+	                    mag=4*classify;
+	                    Set<Integer> singles=new HashSet<>();
+	                    int cur=0;
+	                    for(int k:ranks) {
+    	                    if (cardDict.get(k)==3) mag+=kickers[0]*k;
+    	                    else singles.add(k);}
+    	                return mag+kickers[1]*Collections.max(singles)+kickers[2]*Collections.min(singles);
+	                }
+	            } else if (values.contains(2)) {// One pair or two pairs:
+	            	Set<Integer> pairs=new HashSet<>();
+	            	List<Integer> singles=new ArrayList<>();
+	                for(int k:ranks) {
+	                    if (cardDict.get(k)==2) pairs.add(k);
+	                    else singles.add(k);}
+	                if (pairs.size()==2) return 3*classify+Collections.max(pairs)*kickers[0]+Collections.min(pairs)*kickers[1]+Collections.max(singles)*kickers[2];
+	                // two pairs
+	                else {//one pair
+	                	mag = 2*classify+Collections.max(pairs)*kickers[0];
+	                	Collections.sort(singles);
+	                	for(int i=0;i<3;i++) {
+	                		mag+=singles.get(i)*kickers[3-i];
+	                	}
+	                	return mag;
+	                }
+	            } else {
+	                mag=classify;
+	                int max;
+	        		for(int i=0;i<5;i++){
+		                max=Collections.max(ranks);
+		                ranks.remove(max);
+		                mag=mag+max*kickers[i];
+		            }
+		            return mag;
+	            }
+	        }
+	    }
+	}
+
 	
 	/**
 	 * Calculate if the cards can constitute a string.
@@ -42,10 +149,7 @@ public class FakeEval7 {
 	public int isStraight(List<String> cards) {
 		Map<Integer,Integer> card_dict=this.toMap(cards);
 		Set<Integer> ranks=card_dict.keySet();
-		int smallest=16;
-		for (int i:ranks){
-			if (i < smallest) smallest=i;
-		}
+		int smallest=Collections.min(ranks);
 	    if (smallest==2) {
 	        for (int i=1;i<4;i++)
 	            if (!ranks.contains(smallest+i)) return 0;
--- java_skeleton/src/javabot/test/FakeEval7Test.java
+++ java_skeleton/src/javabot/test/FakeEval7Test.java
@@ -50,4 +50,210 @@ public class FakeEval7Test {
 		List<String> flush=List.of("6s","2c","8h","4s","5d");
         assertFalse(eval7.isFlush(flush),"Expect a non-flush");
     }
+	
+	@Test
+	public void testScoreRoyalFlush() {
+		List<String> cards=List.of("Ts","As","Ks","Js","Qs");
+		assertEquals(eval7.handScore(cards),10<<20,"Expect a True score");
+		
+    }
+	
+	@Test
+	public void testScoreStriaghtFlush() {
+		List<String> cards=List.of("Jc","9c","8c","Tc","7c");
+		assertEquals(eval7.handScore(cards),(9<<20)+(11<<16),"Expect a True score");
+    }
+	
+	@Test
+	public void testScoreStriaghtA5Flush() {
+		List<String> cards=List.of("Ac","2c","5c","4c","3c");
+		assertEquals(eval7.handScore(cards),(9<<20)+(5<<16),"Expect a True score");
+    }
+	
+	@Test
+	public void testScore4OfAKind() {
+		List<String> cards=List.of("6s","6c","6h","4s","6d");
+		assertEquals(eval7.handScore(cards),(8<<20)+(6<<16)+(4<<12),"Expect a True score");
+    }
+	
+	@Test
+	public void testScoreFullHouse() {
+		List<String> cards=List.of("6s","6c","8h","8s","6d");
+		assertEquals(eval7.handScore(cards),(7<<20)+(6<<16)+(8<<12),"Expect a True score");
+    }
+	
+	@Test
+	public void testScoreFlush() {
+		List<String> cards=List.of("6s","7s","8s","4s","Ts");
+		assertEquals(eval7.handScore(cards),(6<<20)+(10<<16)+(8<<12)+(7<<8)+(6<<4)+(4),"Expect a True score");
+    }
+	
+	@Test
+	public void testScoreStraight() {
+		List<String> cards=List.of("6s","7c","8h","9s","Td");
+		assertEquals(eval7.handScore(cards),(5<<20)+(10<<16),"Expect a True score");
+    }
+	
+	@Test
+	public void testScoreA5Straight() {
+		List<String> cards=List.of("As","2c","3h","5s","4d");
+		assertEquals(eval7.handScore(cards),(5<<20)+(5<<16),"Expect a True score");
+    }
+
+	@Test
+	public void testScore3OfAKind() {
+		List<String> cards=List.of("Ts","9c","Th","8s","Td");
+		assertEquals(eval7.handScore(cards),(4<<20)+(10<<16)+(9<<12)+(8<<8),"Expect a True score");
+    }
+
+	@Test
+	public void testScoreTwoPairs() {
+		List<String> cards=List.of("6s","2c","4h","4s","6d");
+		assertEquals(eval7.handScore(cards),(3<<20)+(6<<16)+(4<<12)+(2<<8),"Expect a True score");
+    }
+
+	@Test
+	public void testScoreOnePair() {
+		List<String> cards=List.of("6s","2c","3h","4s","6d");
+		assertEquals(eval7.handScore(cards),(2<<20)+(6<<16)+(4<<12)+(3<<8)+(2<<4),"Expect a True score");
+    }
+	
+	@Test
+	public void testScoreOnePair2() {
+		List<String> cards=List.of("6s","Ac","3h","4s","4d");
+		assertEquals(eval7.handScore(cards),(2<<20)+(4<<16)+(14<<12)+(6<<8)+(3<<4),"Expect a True score");
+    }
+
+	@Test
+	public void testScoreHighCard() {
+		List<String> cards=List.of("6s","2c","8h","4s","5d");
+        assertEquals(eval7.handScore(cards),(1<<20)+(8<<16)+(6<<12)+(5<<8)+(4<<4)+(2),"Expect a True score");
+    }
+	
+	@Test
+	public void testMixingHighCard() {
+		List<String> common=List.of("6s","2c","8h","4s","5d");
+		List<String> ours=List.of("Kc","Ad");
+		List<String> highest=List.of("Kc","Ad","8h","5d","6s");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+	
+	@Test
+	public void testMixingOnePair() {
+		List<String> common=List.of("6s","2c","8h","4s","5d");
+		List<String> ours=List.of("6c","Ad");
+		List<String> highest=List.of("6c","Ad","8h","5d","6s");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+	
+	@Test
+	public void testMixingOnePair2() {
+		List<String> common=List.of("6s","2c","8h","4s","5d");
+		List<String> ours=List.of("Ac","Ad");
+		List<String> highest=List.of("Ac","Ad","8h","5d","6s");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+	
+	@Test
+	public void testMixingTwoairs() {
+		List<String> common=List.of("6s","2c","8h","4s","5d");
+		List<String> ours=List.of("6c","8d");
+		List<String> highest=List.of("6c","8d","8h","5d","6s");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+	
+	@Test
+	public void testMixingThreeOfAKind() {
+		List<String> common=List.of("6s","2c","8h","4s","5d");
+		List<String> ours=List.of("6c","6d");
+		List<String> highest=List.of("6c","5c","8h","6d","6s");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+
+	@Test
+	public void testMixingStraight() {
+		List<String> common=List.of("6s","2c","8h","4s","5d");
+		List<String> ours=List.of("3c","Ac");
+		List<String> highest=List.of("6c","5c","4s","2c","3c");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+
+	@Test
+	public void testMixingFlush() {
+		List<String> common=List.of("6s","2s","8h","4s","5d");
+		List<String> ours=List.of("As","Ks");
+		List<String> highest=List.of("As","Ks","6s","2s","4s");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+
+	@Test
+	public void testMixingFullHouse() {
+		List<String> common=List.of("6s","2c","6h","4s","5d");
+		List<String> ours=List.of("6c","4c");
+		List<String> highest=List.of("6c","6h","4s","4c","6s");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+	
+	@Test
+	public void testMixingFullHouse2() {
+		List<String> common=List.of("6s","6d","6h","4s","4d");
+		List<String> ours=List.of("Ac","Kc");
+        assertEquals(eval7.score(ours, common),eval7.handScore(common),"Expect a True score");
+    }
+	
+	@Test
+	public void testMixingFullHouse3() {
+		List<String> common=List.of("6s","6d","6h","4s","4d");
+		List<String> ours=List.of("Ac","4c");
+        assertEquals(eval7.score(ours, common),eval7.handScore(common),"Expect a True score");
+    }
+	
+	@Test
+	public void testMixingFullHouse4() {
+		List<String> common=List.of("6s","6d","6h","4s","3d");
+		List<String> ours=List.of("Ac","4c");
+		List<String> highest=List.of("6c","6h","4s","4c","6s");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+	
+	@Test
+	public void testMixingFullHouse5() {
+		List<String> common=List.of("6s","6d","4c","4s","3d");
+		List<String> ours=List.of("Ac","6h");
+		List<String> highest=List.of("6c","6h","4s","4c","6s");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+	
+	@Test
+	public void testMixing4OfaKind() {
+		List<String> common=List.of("6s","2c","6h","4s","5d");
+		List<String> ours=List.of("6c","6d");
+		List<String> highest=List.of("6c","6h","5d","6h","6d");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+
+	@Test
+	public void testMixingStraightFlush() {
+		List<String> common=List.of("6s","2d","8h","4d","5d");
+		List<String> ours=List.of("6d","3d");
+		List<String> highest=List.of("6d","3d","2d","5d","4d");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+
+	@Test
+	public void testMixingTwoairs2() {
+		List<String> common=List.of("6s","2c","3c","4c","5c");
+		List<String> ours=List.of("6c","Ac");
+		List<String> highest=List.of("6c","5c","4c","3c","2c");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+
+	@Test
+	public void testMixingRoyalFlush2() {
+		List<String> common=List.of("As","Ks","Qs","Ts","5d");
+		List<String> ours=List.of("6c","Js");
+		List<String> highest=List.of("As","Ts","Qs","Ks","Js");
+        assertEquals(eval7.score(ours, common),eval7.handScore(highest),"Expect a True score");
+    }
+
 }
