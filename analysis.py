affd05cd5ac45a3f89016f73c1ccde238fb01aa8
Shengtong Zhang <szth1555@mit.edu>
2021-01-18 17:11:32 +0800

    update raise_ratio stats

--- analysis.py
+++ analysis.py
@@ -1,4 +1,4 @@
-import matplotlib
+import matplotlib.pyplot as pl
 import re
 filename = "gamelog.txt"
 REGEX_BLIND = "(.) posts the blind of (\d)"
@@ -14,11 +14,14 @@ REGEX_AWARD = "(.) awarded (.*)"
 with open(filename, "r") as f:
     f.readline() 
     f.readline() # discard the header
+    #Various stat trackers
     line = "start"
     line_cnt = 0
     number_of_raise = {'A':0,'B':0}
     total_of_raise = {'A':0,'B':0}
+    raise_ratios = {'A':[],'B':[]}
     number_of_folds = {'A':0,'B':0}
+    preflop_folds = {'A':0, 'B':0}
     all_raises = {'A':[], 'B': []}
     awards = {'A': [], 'B': []}
     #read until EOF
@@ -30,6 +33,7 @@ with open(filename, "r") as f:
         board_pots = [2, 4, 6] # the initial board inflation, blinds included
         round_raises = {'A':[[], [], []], 'B':[[], [], []]}
         streets = [[], [], []]
+        preflop = True
         pips = {'A':[0,0,0], 'B':[0,0,0]}
         line = f.readline()
         while len(line) > 1:
@@ -37,6 +41,7 @@ with open(filename, "r") as f:
             words = line.split(" ")
             #deal with revealing cards
             if words[0] in ["Flop", "Turn", "River"]:
+                preflop = False
                 result = re.match(REGEX_REVEAL, line)
                 cards, amount, board = result.group(2, 3, 6)
                 amount = int(amount)
@@ -67,11 +72,15 @@ with open(filename, "r") as f:
                     player, amount, board = result.group(1, 2, 3)
                     board = int(board) - 1
                     amount = int(amount)
+                    # First discount the cont_cost
+                    opponent = 'B' if player == 'A' else 'A'
+                    cont_cost = max(pips[opponent][board] - pips[player][board], 0)
                     # The amount the player bets before the raise
                     prev_amount = pips[player][board]
                     raise_amount = amount - prev_amount
                     number_of_raise[player] += 1
                     total_of_raise[player] += raise_amount
+                    raise_ratios[player].append((raise_amount - cont_cost)/ (board_pots[board] + cont_cost))
                     # update the pot
                     pips[player][board] = amount
                     board_pots[board] += raise_amount
@@ -81,6 +90,8 @@ with open(filename, "r") as f:
                     player, board = result.group(1, 2)
                     board = int(board) - 1
                     number_of_folds[player] += 1
+                    if preflop:
+                        preflop_folds[player] += 1
                 #calls  
                 elif words[1] == "calls":
                     result = re.match(REGEX_CALL, line)
@@ -115,12 +126,19 @@ with open(filename, "r") as f:
                     pass
             line_cnt += 1
             line = f.readline()
-#print statistics
+#print general statistics
 print("Number of raises:", number_of_raise)
 print("Number of folds:", number_of_folds)
 print("Total amount of raises:", total_of_raise)
+pl.hist(raise_ratios['A'], bins = 40, label="A")
+pl.hist(raise_ratios['B'], bins = 40, label="B")
+pl.legend()
+pl.savefig("raise_ratio.png")
+#print specialize statistics
+print("Number of preflop folds:", preflop_folds)
 for player in total_of_raise:
     print("Average raise of {}: {}".format(player, total_of_raise[player] / number_of_raise[player]))
+#print pre-flop statistics
 #print("Awards:", awards)
 #TODO: pre-flop raise
 #TODO: raise in advantage
